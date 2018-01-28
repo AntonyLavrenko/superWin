5d69700f33aaa62e3c0d92cde42e346b118b4345
Antony Lavrenko <lavrenkolearnnice@gmail.com>
2018-01-28 21:09:00 +0200

    remove temporary files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var main_1 = require("./main");
-document.addEventListener('DOMContentLoaded', function () {
-    window["app"] = new main_1.Main();
-}, false);
-//# sourceMappingURL=index.js.map
