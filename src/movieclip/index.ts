c97d3b01556b9e6024feb1ca3bccdd00ad0d0823
Antony Lavrenko <lavrenkolearnnice@gmail.com>
2018-01-28 22:26:57 +0200

    added assets

---
+++ src/movieclip/index.ts
@@ -0,0 +1,12 @@
+import {EventEmitter} from "eventemitter3";
+
+export class BackgroundMovie extends EventEmitter {
+	constructor(){
+		super();
+	}
+	
+	dependencies(): Promise<any> {
+		
+	}
+}
+	
